077a37636c647cdab1b9d6eee396805858a7e923
YerBrown <yerbrown98@gmail.com>
2024-10-29 19:18:50 +0100

    Añadir imagenes de productos de la tienda

--- JS/CLASS/shopMenu.js
+++ JS/CLASS/shopMenu.js
@@ -1,11 +1,35 @@
 import Menu from "./menu.js";
+import { createHTMLElement, createImgElement } from "../codigo.js";
 class ShopMenu extends Menu {
     constructor(parentId){
         super(parentId);
         
     }
+    createMenu(){
+        const menu = createHTMLElement("div", "shop-menu", ["menu"]);
+    }
     loadMenu(){
         super.loadMenu();
+    }
+    createShopSubmenu(){
+        this.createTopPanel();
+        this.createBottomPanel();
+    }
+    createTopPanel(){
+        const topPanel = createHTMLElement('div', 'shop-top-panel')
+
+        const shopTitle = createHTMLElement('h1', 'shop-title')
+        
+    }
+    createBottomPanel(){
+        this.createFilterBar();
+        this.createProductsViewPanel();
+    }
+    createFilterBar(){
+
+    }
+    createProductsViewPanel(){
+
     }
 }
 export default ShopMenu;
